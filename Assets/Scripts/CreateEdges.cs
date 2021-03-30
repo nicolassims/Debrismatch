05e94f4cccf14df1d20f847e193f05e2c3d01736
nicolassims <sableye.nico@gmail.com>
2021-03-29 20:11:24 -0700

    Mounts are now assigned at runtime based on the number of sides assigned to the script.

--- Assets/Scripts/CreateEdges.cs
+++ Assets/Scripts/CreateEdges.cs
@@ -7,26 +7,36 @@ public class CreateEdges : MonoBehaviour {
     public int numSides;//the number of sides the "placement object" has. This assumes the placement object is a parallelogram
     public GameObject redspot;
 
+    private List<Vector2> GetCorners(float radius) {
+        List<Vector2> points = new List<Vector2>();
+
+        for (var i = 0; i < numSides; i++) {
+            float radians = (2 * Mathf.PI) / numSides * i;
+            float newx = radius * Mathf.Cos(radians) / 100;
+            float newy = radius * Mathf.Sin(radians) / 100;
+            points.Add(new Vector2(newx, newy));
+        }
+
+        return points;
+    }
+
     // Start is called before the first frame update
     void Start() {
         if (numSides < 3) {
             throw new ArgumentException("Number of sides cannot be <3.");
         }
 
-        //create function here that will modify PolygonCollider2D's points to be right place/location, if not already
-        //NOTE: This script will only work on square objects until this function is implemented
+        List<Vector2> points = GetCorners(GetComponent<SpriteRenderer>().sprite.rect.width / 2);
         
         //find the midpoints of each edge and add them together.
-        PolygonCollider2D coll = GetComponent<PolygonCollider2D>();
-        Vector2[] points = coll.points;
         ArrayList midpoints = new ArrayList();
         for (var i = 0; i < numSides - 1; i++) {
             float newfloatx = (points[i].x + points[i + 1].x) / 2;
             float newfloaty = (points[i].y + points[i + 1].y) / 2;
             midpoints.Add(new Vector2(newfloatx, newfloaty));
         }
-        float lastfloatx = (points[0].x + points[points.Length - 1].x) / 2;
-        float lastfloaty = (points[0].y + points[points.Length - 1].y) / 2;
+        float lastfloatx = (points[0].x + points[points.Count - 1].x) / 2;
+        float lastfloaty = (points[0].y + points[points.Count - 1].y) / 2;
         midpoints.Add(new Vector2(lastfloatx, lastfloaty));
 
         //add a red spot on each midpoint
@@ -36,10 +46,5 @@ public class CreateEdges : MonoBehaviour {
             //creating the red spot at the edge of the object. the empty Quaternion is mandatory.
             Instantiate(redspot, v3, new Quaternion());
         }
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
     }
 }
