f4f1b37098032947c63fe424a8d24b8a14a5b570
nicolassims <sableye.nico@gmail.com>
2021-04-10 16:41:56 -0700

    Vending machine now works. Needs to be "turned off" in active mode.

--- Assets/Scripts/VendingScript.cs
+++ Assets/Scripts/VendingScript.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VendingScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class VendingScript : MonoBehaviour {
+    public GameObject[] parts;
+
+    List<string> vended = new List<string>();
+
+    void Start() {
+        for (var i = 0; i < parts.Length; i++) {
+            GameObject newobj = Instantiate(parts[i], new Vector3(-13.5f, 10 - 20 / (parts.Length + 1) * (i + 1)), Quaternion.identity);
+            vended.Add(newobj.name.Replace("(Clone)", ""));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnMouseDown() {
+        List<string> tempvended = new List<string>(vended);
+        foreach (GameObject part in GameObject.FindGameObjectsWithTag("Widget")) {
+            if (part.GetComponent<EditingWidget>().closestMount == null) {
+                tempvended.Remove(part.name.Replace("(Clone)", ""));
+            }
+        }
+        foreach (string name in tempvended) {
+            for (var i = 0; i < parts.Length; i++) {
+                if (parts[i].name.Replace("(Clone)", "") == name) { 
+                    Instantiate(parts[i], new Vector3(-13.5f, 10 - 20 / (parts.Length + 1) * (i + 1)), Quaternion.identity);
+                    break;
+                }
+            }
+        }
     }
 }
